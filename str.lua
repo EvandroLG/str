f0d7f96021550cfa4cf63fbb5d07773e3d7bc5fc
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2016-04-03 22:00:27 -0300

    first commit

---
+++ str.lua
@@ -0,0 +1,45 @@
+local str = {
+    slice = function(s, start, finish)
+    end,
+
+    min = function(s)
+    end,
+
+    max = function(s)
+    end,
+
+    split = function(s, pattern)
+    end,
+
+    trim = function(s) 
+    end,
+
+    trim_right = function(s)
+    end,
+
+    trim_left = function(s)
+    end,
+
+    starts_with = function(s, start, finish)
+    end,
+
+    ends_with = function(s, start, finish)
+    end,
+
+    capitalize = function(s)
+    end,
+
+    is_ascii = function(s)
+    end,
+
+    is_decimal = function(s)
+    end,
+
+    is_number = function(s)
+    end,
+
+    is_digit = function(s)
+    end
+}
+
+return str
---
+++ test.lua
@@ -0,0 +1,19 @@
+local base = require 'base_converter'
+local format = string.format
+
+function test(name, func)
+  xpcall(function()
+    func()
+    print(format('[pass] %s', name))
+  end, function(err)
+    print(format('[fail] %s : %s', name, err))  
+  end)
+end
+
+function _equal(a, b)
+  return a == b
+end
+
+function assert_equal(a, b)
+  assert(_equal(a, b))
+end
