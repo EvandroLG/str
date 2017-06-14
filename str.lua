cdd12672907825270278f17e92028af47b2a23dc
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2017-06-13 22:19:20 -0300

    refactoring

--- str.lua
+++ str.lua
@@ -1,187 +1,189 @@
+-- str
+-- author: Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
+-- https://github.com/evandrolg
+-- License: MIT
+
 local utf8 = require 'lua-utf8'
 
 local function to_bool(value)
-    return not not value
+  return not not value
 end
 
 local function get_list_chars(s)
-    local output = {}
+  local output = {}
 
-    for k, c in utf8.codes(s) do
-        table.insert(output, utf8.char(c))
+  for k, c in utf8.codes(s) do
+    table.insert(output, utf8.char(c))
+  end
+
+  return output
+end
+
+local str
+
+str = {
+  __VERSION = '1.4.0',
+  __DESCRIPTION = '',
+  __LICENCE = [[
+    The MIT License (MIT)
+
+    Copyright (c) 2017 Evandro Leopoldino Gonçalves
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+  ]],
+
+  slice = function(s, start, finish)
+    return string.sub(s, start, finish or #s)
+  end,
+
+  starts_with = function(s, start)
+    return string.sub(s, 1, #start) == start
+  end,
+
+  ends_with = function(s, finish)
+    return string.sub(s, -#finish) == finish
+  end,
+
+  count = function(s, substr)
+    local total = 0
+    local start = nil
+    local finish = 1
+
+    repeat
+      start, finish = string.find(s, substr, finish, true)
+      if start then total = total + 1 end
+    until start == nil
+
+    return total
+  end,
+
+  split = function(s, pattern)
+    local output = {}
+    local fpat = '(.-)' .. pattern
+    local last_end = 1
+    local _s, e, cap = s:find(fpat, 1)
+
+    while _s do
+      if _s ~= 1 or cap ~= '' then
+        table.insert(output, cap)
+      end
+
+      last_end = e+1
+      _s, e, cap = s:find(fpat, last_end)
+    end
+
+    if last_end <= #s then
+      cap = s:sub(last_end)
+      table.insert(output, cap)
     end
 
     return output
-end
+  end,
 
-local str = {
-    __VERSION = '1.4.0',
-    __DESCRIPTION = '',
-    __LICENCE = [[
-        The MIT License (MIT)
+  trim_right = function(s)
+    return string.match(s, '(.-)%s*$')
+  end,
 
-        Copyright (c) 2016 Evandro Leopoldino Gonçalves
+  trim_left = function(s)
+    return string.match(s, '[^%s+].*')
+  end,
 
-        Permission is hereby granted, free of charge, to any person obtaining a copy
-        of this software and associated documentation files (the "Software"), to deal
-        in the Software without restriction, including without limitation the rights
-        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-        copies of the Software, and to permit persons to whom the Software is
-        furnished to do so, subject to the following conditions:
+  trim = function(s)
+    return str.trim_right(str.trim_left(s))
+  end,
 
-        The above copyright notice and this permission notice shall be included in all
-        copies or substantial portions of the Software.
-
-        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-        SOFTWARE.
-    ]]
-}
-
-str = {
-    slice = function(s, start, finish)
-        return string.sub(s, start, finish or #s)
-    end,
-
-    starts_with = function(s, start)
-        return string.sub(s, 1, #start) == start
-    end,
-
-    ends_with = function(s, finish)
-        return string.sub(s, -#finish) == finish
-    end,
-
-    count = function(s, substr)
-        local total = 0
-        local start = nil
-        local finish = 1
-
-        repeat
-            start, finish = string.find(s, substr, finish, true)
-
-            if start then
-                total = total + 1
-            end
-        until start == nil
-
-        return total
-    end,
-
-    split = function(s, pattern)
-        local output = {}
-        local fpat = '(.-)' .. pattern
-        local last_end = 1
-        local _s, e, cap = s:find(fpat, 1)
-
-        while _s do
-            if _s ~= 1 or cap ~= '' then
-                table.insert(output, cap)
-            end
-
-            last_end = e+1
-            _s, e, cap = s:find(fpat, last_end)
-        end
-
-        if last_end <= #s then
-            cap = s:sub(last_end)
-            table.insert(output, cap)
-        end
-
-        return output
-    end,
-
-    trim_right = function(s)
-        return string.match(s, '(.-)%s*$')
-    end,
-
-    trim_left = function(s)
-        return string.match(s, '[^%s+].*')
-    end,
-
-    trim = function(s)
-        return str.trim_right(str.trim_left(s))
-    end,
-
-    capitalize = function(s)
-        if #s == 0 then
-            return s
-        end
-
-        local output = {}
-        output[1] = string.upper(string.sub(s, 1, 1))
-
-        for i=2, #s do
-            local character = string.sub(s, i, i)
-            table.insert(output, string.lower(character))
-        end
-
-        return table.concat(output)
-    end,
-
-    center = function(s, n)
-        local aux = ''
-
-        for i=1, n do
-            aux = aux .. ' '
-        end
-
-        return aux .. s .. aux
-    end,
-
-    slug = function(s)
-        local splited = get_list_chars(s)
-        local output = {}
-        local accents = [[ÀÁÂÃÄÅàáâãäåÒÓÔÕÕÖØòóôõöøÈÉÊËèéêëðÇçÐÌÍÎÏìíîï
-                        ÙÚÛÜùúûüÑñŠšŸÿýŽž~"\'~^!?.:@#$%&*]]
-        local accents_out = [[AAAAAAaaaaaaOOOOOOOooooooEEEEeeeeeCcDIIIIiiii
-                            UUUUuuuuNnSsYyyZz---------------]]
-
-        for key, value in pairs(splited) do
-            local index_of = utf8.find(accents, value)
-
-            if index_of then
-                output[key] = utf8.sub(accents_out, index_of, index_of)
-            else
-                output[key] = value
-            end
-        end
-
-        return utf8.lower(table.concat(output, ''):gsub('%s', '-'))
-    end,
-
-    is_ascii = function(s)
-        for i=1, #s do
-            if string.byte(s:sub(i, i)) > 126 then return false end
-        end
-
-        return true
-    end,
-
-    is_number = function(s)
-        return to_bool(string.find(s, '^%d+$'))
-    end,
-
-    each_char = function(s, callback)
-        for i=1, #s do
-            callback(s:sub(i, i))
-        end
-    end,
-
-    each_byte = function(s, callback)
-        str.each_char(s, function(char)
-            callback(char:byte())
-        end)
-    end,
-
-    each_line = function(s, callback)
-        for line in string.gmatch(s, '[^\n]+') do
-            callback(line)
-        end
+  capitalize = function(s)
+    if #s == 0 then
+      return s
     end
+
+    local output = {}
+    output[1] = string.upper(string.sub(s, 1, 1))
+
+    for i=2, #s do
+      local character = string.sub(s, i, i)
+      table.insert(output, string.lower(character))
+    end
+
+    return table.concat(output)
+  end,
+
+  center = function(s, n)
+    local aux = ''
+
+    for i=1, n do
+      aux = aux .. ' '
+    end
+
+    return aux .. s .. aux
+  end,
+
+  slug = function(s)
+    local splited = get_list_chars(s)
+    local output = {}
+    local accents = [[ÀÁÂÃÄÅàáâãäåÒÓÔÕÕÖØòóôõöøÈÉÊËèéêëðÇçÐÌÍÎÏìíîï
+    ÙÚÛÜùúûüÑñŠšŸÿýŽž~"\'~^!?.:@#$%&*]]
+    local accents_out = [[AAAAAAaaaaaaOOOOOOOooooooEEEEeeeeeCcDIIIIiiii
+    UUUUuuuuNnSsYyyZz---------------]]
+
+    for key, value in pairs(splited) do
+      local index_of = utf8.find(accents, value)
+
+      if index_of then
+        output[key] = utf8.sub(accents_out, index_of, index_of)
+      else
+        output[key] = value
+      end
+    end
+
+    return utf8.lower(table.concat(output, ''):gsub('%s', '-'))
+  end,
+
+  is_ascii = function(s)
+    for i=1, #s do
+      if string.byte(s:sub(i, i)) > 126 then return false end
+    end
+
+    return true
+  end,
+
+  is_number = function(s)
+    return to_bool(string.find(s, '^%d+$'))
+  end,
+
+  each_char = function(s, callback)
+    for i=1, #s do
+      callback(s:sub(i, i))
+    end
+  end,
+
+  each_byte = function(s, callback)
+    str.each_char(s, function(char)
+      callback(char:byte())
+    end)
+  end,
+
+  each_line = function(s, callback)
+    for line in string.gmatch(s, '[^\n]+') do
+      callback(line)
+    end
+  end
 }
 
 return str
--- test.lua
+++ test.lua
@@ -1,148 +1,147 @@
 local str = require 'str'
-local format = string.format
 
-function test(name, func)
-  xpcall(function()
-    func()
-    print(format('[pass] %s', name))
-  end, function(err)
-    print(format('[fail] %s : %s', name, err))  
-  end)
-end
+do
+  local format = string.format
 
-function _equal(a, b)
-  return a == b
+  function test(name, func)
+    xpcall(function()
+      func()
+      print(format('[pass] %s', name))
+    end, function(err)
+      print(format('[fail] %s : %s', name, err))
+    end)
+  end
 end
 
 function assert_equal(a, b)
-  assert(_equal(a, b))
+  assert(a == b)
 end
 
 test('slice should returns "javascript"', function()
-   assert_equal(str.slice('lua, javascript, python', 6, 15), 'javascript')
+  assert_equal(str.slice('lua, javascript, python', 6, 15), 'javascript')
 end)
 
 test('slice should returns "javascript, python"', function()
-   assert_equal(str.slice('lua, javascript, python', 6), 'javascript, python')
+  assert_equal(str.slice('lua, javascript, python', 6), 'javascript, python')
 end)
 
 test('starts_with should returns true', function()
-    assert_equal(str.starts_with('lua is a great language', 'lua'), true)
+  assert_equal(str.starts_with('lua is a great language', 'lua'), true)
 end)
 
 test('starts_with should returns true', function()
-    assert_equal(str.starts_with('lua is a great language', 'ruby'), false)
+  assert_equal(str.starts_with('lua is a great language', 'ruby'), false)
 end)
 
 test('ends_with  should returns true', function()
-    assert_equal(str.ends_with('lua is a great language', 'language'), true)
+  assert_equal(str.ends_with('lua is a great language', 'language'), true)
 end)
 
 test('ends_with should returns false', function()
-    assert_equal(str.ends_with('lua is a great language', 'programming'), false)
+  assert_equal(str.ends_with('lua is a great language', 'programming'), false)
 end)
 
 test('is_ascii should returns true', function()
-    assert_equal(str.is_ascii('lua is a great language'), true)
+  assert_equal(str.is_ascii('lua is a great language'), true)
 end)
 
 test('is_ascii should returns false', function()
-    assert_equal(str.is_ascii('á'), false)
+  assert_equal(str.is_ascii('á'), false)
 end)
 
 test('capitalize should returns string with first uppercase character', function()
-    assert_equal(str.capitalize('lua is a great language'), 'Lua is a great language')
+  assert_equal(str.capitalize('lua is a great language'), 'Lua is a great language')
 end)
 
 test('count should returns 0', function()
-    assert_equal(str.count('lua is a great language. lua is a brazilian language', 'python'), 0)
+  assert_equal(str.count('lua is a great language. lua is a brazilian language', 'python'), 0)
 end)
 
 test('count should returns how many times the substring was found', function()
-    assert_equal(str.count('lua is a great language. lua is a brazilian language', 'lua'), 2)
+  assert_equal(str.count('lua is a great language. lua is a brazilian language', 'lua'), 2)
 end)
 
 test('trim_left should returns string without left blank spaces', function()
-    assert_equal(str.trim_left('  lua is a great language'), 'lua is a great language')
+  assert_equal(str.trim_left('  lua is a great language'), 'lua is a great language')
 end)
 
 test('trim_right should returns string without right blank spaces', function()
-    assert_equal(str.trim_right('lua is a great language  '), 'lua is a great language')
+  assert_equal(str.trim_right('lua is a great language  '), 'lua is a great language')
 end)
 
 test('trim should returns string with complete trim', function()
-    assert_equal(str.trim('  lua is a great language  '), 'lua is a great language')
+  assert_equal(str.trim('  lua is a great language  '), 'lua is a great language')
 end)
 
 test('is_number should returns true', function()
-    assert_equal(str.is_number('12345'), true)
+  assert_equal(str.is_number('12345'), true)
 end)
 
 test('is_number should returns false', function()
-    assert_equal(str.is_number('1234a5'), false)
+  assert_equal(str.is_number('1234a5'), false)
 end)
 
 test('is_number should returns false when value is empty', function()
-    assert_equal(str.is_number(''), false)
+  assert_equal(str.is_number(''), false)
 end)
 
 test('center should returns centralized string', function()
-    assert_equal(str.center('lua', 3), '   lua   ')
+  assert_equal(str.center('lua', 3), '   lua   ')
 end)
 
 test('split should returns a table resultant from the splited string', function()
-    local result = str.split('lua is a great language', ' ')
+  local result = str.split('lua is a great language', ' ')
 
-    assert_equal(#result, 5)
-    assert_equal(result[1], 'lua')
-    assert_equal(result[2], 'is')
-    assert_equal(result[3], 'a')
-    assert_equal(result[4], 'great')
-    assert_equal(result[5], 'language')
+  assert_equal(#result, 5)
+  assert_equal(result[1], 'lua')
+  assert_equal(result[2], 'is')
+  assert_equal(result[3], 'a')
+  assert_equal(result[4], 'great')
+  assert_equal(result[5], 'language')
 end)
 
 test('slug should returns a new lowercase string and without spaces', function()
-    local result = str.slug('Lua is a great language')
-    assert_equal(result, 'lua-is-a-great-language')
+  local result = str.slug('Lua is a great language')
+  assert_equal(result, 'lua-is-a-great-language')
 end)
 
 test('slug should returns a new string without accents', function()
-    local result = str.slug('Lua é uma ótima linguagem')
-    assert_equal(result, 'lua-e-uma-otima-linguagem')
+  local result = str.slug('Lua é uma ótima linguagem')
+  assert_equal(result, 'lua-e-uma-otima-linguagem')
 end)
 
 test('each_char should returns every character of the string', function()
-    local result = {}
-    str.each_char('lua', function(char)
-        table.insert(result, char)
-    end)
+  local result = {}
+  str.each_char('lua', function(char)
+    table.insert(result, char)
+  end)
 
-    assert_equal(#result, 3)
-    assert_equal(result[1], 'l')
-    assert_equal(result[2], 'u')
-    assert_equal(result[3], 'a')
+  assert_equal(#result, 3)
+  assert_equal(result[1], 'l')
+  assert_equal(result[2], 'u')
+  assert_equal(result[3], 'a')
 end)
 
 test('each_byte should returns the byte of every character of the string', function()
-    local result = {}
-    str.each_byte('lua', function(char)
-        table.insert(result, char)
-    end)
+  local result = {}
+  str.each_byte('lua', function(char)
+    table.insert(result, char)
+  end)
 
-    assert_equal(#result, 3)
-    assert_equal(result[1], 108)
-    assert_equal(result[2], 117)
-    assert_equal(result[3], 97)
+  assert_equal(#result, 3)
+  assert_equal(result[1], 108)
+  assert_equal(result[2], 117)
+  assert_equal(result[3], 97)
 end)
 
 test('each_line should returns every line of the string', function()
-    local result = {}
-    str.each_line('lua\nruby\npython', function(line)
-        table.insert(result, line)
-    end)
+  local result = {}
+  str.each_line('lua\nruby\npython', function(line)
+    table.insert(result, line)
+  end)
 
-    assert_equal(#result, 3)
-    assert_equal(result[1], 'lua')
-    assert_equal(result[2], 'ruby')
-    assert_equal(result[3], 'python')
+  assert_equal(#result, 3)
+  assert_equal(result[1], 'lua')
+  assert_equal(result[2], 'ruby')
+  assert_equal(result[3], 'python')
 end)
